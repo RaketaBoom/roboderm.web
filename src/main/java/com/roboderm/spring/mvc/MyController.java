e8a6d8cdce3316cb2610430ab1eb8a570a3bafd4
RaketaBoom <77796518+RaketaBoom@users.noreply.github.com>
2024-03-17 17:32:10 +0400

    Init commit: added dependencies and if run tomcat will show Roboderm on site

---
+++ src/main/java/com/roboderm/spring/mvc/MyController.java
@@ -0,0 +1,13 @@
+package com.roboderm.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+
+    @RequestMapping("/")
+    public String showHomePage(){
+        return "home";
+    }
+}
